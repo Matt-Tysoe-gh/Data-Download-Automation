c9a5be20770ed4de7e1aef2b9d5aaae1db180e6c
Matthew Tysoe <37555494+DavePasta@users.noreply.github.com>
2025-02-14 22:56:48 +0000

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,9 @@
+OUTLOOK_FOLDER_INDEX = 6
+EMAIL_SUBJECTS = {
+    "Review Export": "review",
+    "Catalog Export": "catalog"
+}
+URL_PATTERN = r"download url pattern"
+FILE_PREFIX = ""
+DOWNLOAD_FOLDER = "Downloads"
+RECIPIENT_EMAIL = ""
