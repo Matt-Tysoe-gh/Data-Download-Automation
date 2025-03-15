c8d0d8495676429a615da609e47ad92919f259cf
Matthew Tysoe <37555494+Matt-Tysoe-gh@users.noreply.github.com>
2025-03-15 13:48:09 +0000

    Update main.py
    updated wrong repo...

--- main.py
+++ main.py
@@ -1,123 +1,218 @@
-from flask import Flask
-import pyodbc
-import praw
-from datetime import datetime, timezone
-from typing import List, Dict, Any
-import logging
-
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(asctime)s %(levelname)s: %(message)s",
-    datefmt="%d-%m-%Y %H:%M:%S"
-)
-
-def launch_flask() -> Flask:
-    logging.debug("launching flask")
-    try:
-        app = Flask(__name__)
-        logging.info("flask launched successfully")
-    except Exception:
-        logging.exception("flask failed to start")
-    return app
-
-def praw_setup() -> praw.Reddit:
-    logging.debug("setting up praw")
-    try:
-        reddit = praw.Reddit(
-            client_id='',
-            client_secret='',
-            user_agent=''
-            )
-        logging.info("praw setup completed")
-    except Exception:
-        logging.exception(f"praw setup failed")
-    return reddit
-
-def connect_string() -> str:
-    logging.debug("configuring connection string")
-    try:
-        connection_string = ('')
-        logging.info("connection string configured")
-    except Exception:
-        logging.exception("connection string configuration failed")
-    return connection_string
-
-def subreddit_filters() -> List[str]:
-        logging.debug("setting up subreddit list")
-        try:
-            subreddit_list = [
-            "programming", "learnprogramming", "datascience", "machinelearning",
-            "python", "SQL", "coding", "opensource", "webdev", "dataisbeautiful"
-            ]
-            logging.info(f"subreddit filters: {subreddit_list}")
-        except Exception:
-            logging.exception(f"subreddit list failed. {subreddit_list}")
-        return subreddit_list
-
-def fetch_subreddit_data(subreddit_list: List[str], reddit: praw.Reddit) -> List[Dict[str, Any]]:
-    logging.debug("fetching subreddit data")
-    try:
-        cutoff_timestamp = int(datetime.now(timezone.utc).timestamp() - 3600)
-        return [
-            {
-                "id": post.id,
-                "subreddit": sub,
-                "title": post.title,
-                "upvotes": post.ups,
-                "upvote_ratio": post.upvote_ratio,
-                "comments": post.num_comments,
-                "created_utc": datetime.fromtimestamp(post.created_utc, timezone.utc)
-            }
-            for sub in subreddit_list
-            for post in reddit.subreddit(sub).new()
-            if post.created_utc <= cutoff_timestamp
-        ]
-    except Exception:
-        logging.exception("subreddit post failed")
-
-def post_to_database(posts: List[Dict[str, Any]], cursor: pyodbc):
-    logging.debug("posting data to db")
-    try:
-        for post in posts:
-            logging.info(f"posting: {post}")
-            cursor.execute(
-                """
-                MERGE reddit_posts AS target
-                USING (SELECT ? AS id, ? AS subreddit, ? AS title, ? AS upvotes, ? AS upvote_ratio, ? AS comments, ? AS created_utc) AS source
-                ON target.id = source.id
-                WHEN NOT MATCHED THEN
-                    INSERT (id, subreddit, title, upvotes, upvote_ratio, comments, created_utc)
-                    VALUES (source.id, source.subreddit, source.title, source.upvotes, source.upvote_ratio, source.comments, source.created_utc);
-                """,
-                (post['id'], post['subreddit'], post['title'], post['upvotes'], post['upvote_ratio'], post['comments'], post['created_utc'])
-            )
-        logging.info(f"posted {len(posts)} posts to db successfully")
-    except Exception:
-        logging.exception("failed to post data to db")
-
-def register_routes(app: Flask) -> None:
-    logging.debug("registering routes")
-    @app.route('/', methods=['GET'])
-    def run_etl() -> str:
-        try:
-            logging.debug("running ETL pipeline")
-            reddit = praw_setup()
-            connection_string = connect_string()
-            subreddit_list = subreddit_filters()
-            
-            posts = fetch_subreddit_data(subreddit_list, reddit)
-
-            with pyodbc.connect(connection_string) as conn:
-                with conn.cursor() as cursor:
-                    post_to_database(posts, cursor)
-                    conn.commit()
-            return "ETL process complete"
-        except Exception:
-            logging.exception("ETL pipeline failed")
-            return "ETL process failed", 500
-
-if __name__ == '__main__':
-    logging.debug("starting app")
-    app = launch_flask()
-    register_routes(app)
-    app.run(host='0.0.0.0', port=5000)
+import win32com.client
+ import re
+ import webbrowser
+ import os
+ import sys
+ import threading
+ import pandas as pd
+ from datetime import datetime
+ from typing import Tuple, List, Dict
+ import warnings
+ import logging
+ import constants
+ 
+ logging.basicConfig(
+     level=logging.DEBUG,
+     format="%(asctime)s %(levelname)s: %(message)s",
+     datefmt="%d-%m-%Y %H:%M:%S"
+ )
+ 
+ def initialise_win32com(outlook_folder_index: int) -> Tuple[object, object]:
+     logging.info("initialising win32com Outlook application")
+     try:
+         outlook = win32com.client.Dispatch("Outlook.Application")
+         namespace = outlook.GetNamespace("MAPI")
+         inbox = namespace.GetDefaultFolder(outlook_folder_index)
+         logging.info("Outlook initialised successfully")
+         return inbox, outlook
+     except Exception:
+         logging.exception("Failed to initialise win32com Outlook application")
+         raise
+ 
+ 
+ def get_and_download_emails(inbox: object, filter_list: List[str], email_subjects: Dict[str, str], url_pattern: str, file_prefix: str, download_folder: str) -> Tuple[Dict[str, Dict[str, str]], str]:
+ 
+     logging.info("Fetching and downloading emails")
+     mapping = email_subjects
+     dictionary = {}
+     for subject in filter_list:
+         logging.debug(f"Processing subject: {subject}")
+         email_filter = f"[Subject] = '{subject}'"
+         try:
+             email_messages = inbox.Items.Restrict(email_filter)
+             latest_email = email_messages.GetLast()
+             logging.debug(f"Got latest email for subject: {subject}")
+         except Exception:
+             logging.exception(f"Error finding subject: {subject}")
+             continue
+ 
+         url_match = re.search(rf"{url_pattern}[^\s<>\"']+", latest_email.body)
+         if not url_match:
+             logging.error(f"Error extracting URL: {url_match}")
+             continue
+         url = url_match.group(0)
+         logging.debug(f"Extracted URL: {url}")
+ 
+         webbrowser.open(url)
+         logging.info("Opening URL in browser")
+ 
+         _, _, partition = url.partition(file_prefix)
+         downloads = os.path.join(os.path.expanduser("~"), download_folder)
+         file_name = file_prefix + partition
+         file_path = os.path.join(downloads, file_name)
+         logging.debug(f"Constructed file path: {file_path}")
+ 
+         if subject in mapping:
+             key = mapping[subject]
+             dictionary[key] = {
+                 "urls": url,
+                 "file_paths": file_path
+             }
+             logging.info(f"Mapping dict for {subject} and {key}")
+     return dictionary, downloads
+ 
+ 
+ def wait_for_file(file_path: str, max_attempts: int = 1200) -> bool:
+     logging.info(f"Waiting for file: {file_path}")
+     event = threading.Event()
+     for attempt in range(max_attempts):
+         if os.path.exists(file_path):
+             logging.info(f"File found after {attempt / 10:.1f}s")
+             return True
+         else:
+             logging.debug(f"Waiting for file... {attempt / 10:.1f}s")
+             sys.stdout.flush()
+             event.wait(0.1)
+     logging.error("File not found within maximum allowed time... Try again...")
+     return False
+ 
+ 
+ def reviews_transformations(weeknum: int, dictionary: Dict[str, Dict[str, str]], downloads: str) -> str:
+     logging.info("Starting reviews file transformations")
+     try:
+         df_review = pd.read_excel(dictionary["review"]["file_paths"])
+         logging.info("Reviews file read successfully")
+     except(FileNotFoundError, PermissionError):
+         logging.exception("Error reading reviews file")
+         raise
+ 
+     try:
+         df_review.columns = df_review.iloc[6]
+         df_review = df_review.iloc[8:]
+         df_review['Review Submission Date'] = pd.to_datetime(df_review['Review Submission Date'])
+         df_review.insert(2, 'Purchase Date', df_review['Review Submission Date'] - pd.Timedelta(days=1))
+         df_review['Review Text'] = df_review['Review Text'].apply(lambda x: f'"{x}"')
+         df_review.insert(2, 'email', 'email@email.com')
+         df_review.insert(6, 'blank 1', None)
+         df_review.insert(7, 'blank 2', None)
+         logging.debug("Transformations applied to reviews file")
+     except Exception:
+         logging.exception("Could not apply transformations to reviews file")
+         raise
+ 
+     review_file_path = os.path.join(downloads, f'filename_reviews{weeknum}.xlsx')
+ 
+     try:
+         df_review.to_excel(review_file_path, index=False)
+         logging.info(f"Reviews data written to {review_file_path}")
+     except(FileNotFoundError, PermissionError):
+         logging.exception("Error writing reviews Excel file")
+         raise
+     return review_file_path
+ 
+ 
+ def catalog_transformations(weeknum: int, dictionary: Dict[str, Dict[str, str]], downloads: str) -> str:
+     logging.info("Starting catalog file transformations")
+     try:
+         df_catalog = pd.read_excel(dictionary["catalog"]["file_paths"])
+     except(FileNotFoundError, PermissionError):
+         logging.exception("Error reading catalog file")
+         raise
+ 
+     try:
+         df_catalog.columns = df_catalog.iloc[5]
+         df_catalog = df_catalog.iloc[6:]
+         df_catalog['EAN'] = df_catalog['EAN'].str.split(',').str[0]
+     except Exception:
+         logging.exception("Could not apply transformations to catalog file")
+         raise
+ 
+     catalog_file_path = os.path.join(downloads, f'filename_catalog{weeknum}.xlsx')
+ 
+     try:
+         df_catalog.to_excel(catalog_file_path, index=False)
+         logging.info(f"Catalog data written to {catalog_file_path}")
+     except(FileNotFoundError, PermissionError):
+         logging.exception("Error writing catalog Excel file")
+     return catalog_file_path
+ 
+ 
+ def send_email(outlook: object, pd_file_paths: List[str], weeknum: int, year: int, recipient_email: str) -> bool:
+     if len(pd_file_paths) != 2:
+         logging.warning("Insufficient attachments available. Email will not be sent")
+         return False
+ 
+     logging.info("Preparing email to be sent")
+     try:
+         mail = outlook.CreateItem(0)
+         mail.To = recipient_email
+         mail.Subject = f'Review Data Export Week {weeknum} Year {year}'
+         mail.Body = f'''
+         Hi,
+ 
+         Email body goes here! it is currenly {year} week {weeknum}
+ 
+         Thanks,
+         user        
+         '''
+ 
+         for attachment in pd_file_paths:
+             logging.debug(f"Attaching file: {attachment}")
+             mail.Attachments.Add(attachment)
+ 
+         mail.Send()
+         logging.info("Email sent successfully")
+         return True
+     except Exception:
+         logging.exception("Error occurred while sendin email.")
+         return False
+ 
+ 
+ if __name__ == "__main__":
+     """
+     main guard for code/function organisation, also initialises empty lists and filter lists
+     """
+     try:
+         warnings.filterwarnings("ignore", message="Workbook contains no default style, apply openpyxl's default")
+         logging.info("Script started")
+ 
+         filter_list = ["", ""]
+         pd_file_paths = []
+ 
+         weeknum = f"{datetime.now().isocalendar()[1]-1:02d}"
+         year = f"{datetime.now().isocalendar()[0]}"
+         logging.debug(f"Week number: {weeknum}, Year: {year}")
+         
+         inbox, outlook = initialise_win32com(constants.OUTLOOK_FOLDER_INDEX)
+ 
+         dictionary, downloads = get_and_download_emails(inbox, filter_list, constants.EMAIL_SUBJECTS, constants.URL_PATTERN, constants.FILE_PREFIX, constants.DOWNLOAD_FOLDER)
+ 
+         if wait_for_file(dictionary["review"]["file_paths"]):
+             review_file_path = reviews_transformations(weeknum, dictionary, downloads)
+             pd_file_paths.append(review_file_path)
+         else:
+             raise FileNotFoundError("File not found")
+ 
+         if wait_for_file(dictionary["catalog"]["file_paths"]):
+             catalog_file_path = catalog_transformations(weeknum, dictionary, downloads)
+             pd_file_paths.append(catalog_file_path)
+         else:
+             raise FileNotFoundError("File not found")
+ 
+         sent = send_email(outlook, pd_file_paths, weeknum, year, constants.RECIPIENT_EMAIL)
+         if sent:
+             logging.info("Script has completed successfully")
+         else:
+             logging.error("Script compeleted, but the email was not sent due to missing attachments or an error")
+     except Exception:
+         logging.exception("An unhandled error has occurred in the main script")
